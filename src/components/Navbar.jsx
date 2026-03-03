104531a254fac466022fe5e1e308986eda0abff9
Imran2122 <imranhasanbd212@gmail.com>
2026-03-03 23:09:14 +0600

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { FaPlus } from "react-icons/fa6";
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">CS — Ticket System</a>
+      </div>
+      <div className="flex-none justify-center items-center">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a>Home</a>
+          </li>
+          <li>
+            <a>FAQ</a>
+          </li>
+          <li>
+            <a>Changelog</a>
+          </li>
+          <li>
+            <a>Blog</a>
+          </li>
+          <li>
+            <a>Download</a>
+          </li>
+          <li>
+            <a>Contact</a>
+          </li>
+          <button className="btn bg-gradient-to-r from-purple-600 to-fuchsia-500">
+            <FaPlus /> New Ticket
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
